a4a96bcdaa0cb2aeea8749908276c34caacb0d37
Sebastian <sebastian.thorneus@gmail.com>
2022-11-16 22:37:18 +0100

    ändrat personligt brev till biografi

--- src/pages/Personal.jsx
+++ src/pages/Personal.jsx
@@ -16,14 +16,14 @@ function Personal() {
         <span className={styles.icons}><GiPositionMarker /> <span className={styles.number}>Spannvägen 11 16835 Bromma</span></span>
         <div className={styles.border}></div>
     </div>
-    <div className={styles.experience}><h1 className={styles.experienceText}>Personligt brev</h1></div>
+    <div className={styles.experience}><h1 className={styles.experienceText}>Biografi</h1></div>
     <div className={styles.border}></div>  
     <div className={styles.experienceBox}>
         <p className={styles.bread}>
-        Mitt namn är Sebastian Torneus och jag går just nu utbildningen Frontend /App Developer hos Changemaker Educations. Jag är 32 år gammal och har under större delen av mitt arbetsliv kört lastbil för Home2you i Stockholm. Senaste åren har jag bott en del i Jukkasjärvi där jag renoverat hus som gått i arv och startat upp en uthyrningsverksamhet där jag bland annat hyr ut hus till ishotellet i Jukkasjärvi. Periodvis när jag bott i Jukkasjärvi har jag även arbetat för Postnord i Kiruna, på deras avdelning för pakethantering som är separerad från brevbärar-avdelningen.
+        Mitt namn är Sebastian Torneus och jag går just nu utbildningen Frontend /App Developer hos Changemaker Educations. Jag är 32 år gammal och har under större delen av mitt arbetsliv kört lastbil för Home2you i Stockholm. Senaste åren har jag bott en del i Jukkasjärvi där jag renoverat hus som gått i arv och startat upp en uthyrningsverksamhet där jag bland annat hyr ut hus till ishotellet i Jukkasjärvi. Periodvis när jag bott i Jukkasjärvi har jag även arbetat för Postnord i Kiruna, på deras avdelning för pakethantering som är separerad från brevbärar-avdelningen. I yngre ålder har jag jobbat med diverse ströjobb som publikvärd för Hammarby Fotboll och Solvalla med mera.  
 <br></br>
 <br></br>
-För 1,5 år sedan påbörjade jag utbildningen Frontend/App developer för att jag var intresserad av att byta bransch och testa på något nytt. Av den anledningen är jag just nu ute efter en LIA plats. Jag har under utbildningen gått dessa kurser:
+Under min utbildning hos Changemaker har jag fått lära mig Javascript, CSS och HTML, men vi har även lagt väldigt mycket tid på React. Besök gärna mitt portfolio länkat högst upp på sidan för att se lite av mina sidoprojekt, majoriteten byggda i React. Via portfolion kan ni även nå min Github där jag har lite andra publika repos som skolprojekt, mailserver och annat jag byggt senaste åren. 
 <br></br>
 
 
