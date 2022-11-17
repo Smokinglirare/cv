0dd20b22686f5c06c7e4f6f3ed54fa6049a65209
Sebastian Torneus <Sebastian.thorneus@gmail.com>
2022-11-17 12:08:00 +0100

    fixat hovereffekt på ikonerna

--- src/pages/PersonalLetter.jsx
+++ src/pages/PersonalLetter.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from "./Home.module.css"
 import {TbOctagon} from "react-icons/tb"
 import {SiJavascript, SiReact, SiCss3, SiTypescript} from "react-icons/si"
@@ -6,8 +6,24 @@ import {FaNodeJs, FaGitSquare} from "react-icons/fa"
 import profile from "../images/Sebbe2.png"
 import Personal from './Personal'
 
+
+function useHover() {
+  const [hovering, setHovering] = useState(false)
+  const onHoverProps = {
+    onMouseEnter: () => setHovering(true),
+    onMouseLeave: () => setHovering(false),
+  }
+  return [hovering, onHoverProps]
+}
+
 function PersonalLetter() {
+
+  const [buttonAIsHovering, buttonAHoverProps] = useHover()
+
   return (
+
+  
+
     <div className={styles.container}>
         <div className={styles.grid}>
         <div className={styles.leftSide}>
@@ -19,12 +35,12 @@ function PersonalLetter() {
   <h2 className={styles.me}>Om mig</h2>
   <div className={styles.border}></div>
   <p className={styles.bread}>Jag är en engagerad och prestigelös person som just nu utbildar mig inom frontend-utveckling och senaste åren lärt mig UI och UX design. Jag har arbetat mycket med CSS och Javascript, framför allt på slutet mycket i ReactJS. Jag gillar att grotta ner mig i detaljer och lära mig nya saker. </p>
-  <div className={styles.icons}><span className={styles.icon}><SiJavascript /><span className={styles.iconText}>JS</span></span>
-       <span className={styles.icon}><SiReact /><span className={styles.iconText}>ReactJS</span></span>
-       <span className={styles.icon}><SiCss3 /><span className={styles.iconText}>CSS</span></span>
-       <span className={styles.icon}><SiTypescript /><span className={styles.iconText}>TS</span></span>
-       <span className={styles.icon}><FaNodeJs /><span className={styles.iconText}>NodeJS</span></span>
-       <span className={styles.icon}><FaGitSquare /><span className={styles.iconText}>Git</span></span>
+  <div className={styles.icons}><span className={styles.icon} {...buttonAHoverProps}><SiJavascript />{buttonAIsHovering ? <span className={styles.iconText}>70%</span> : <span className={styles.iconText}>JS</span>} </span>
+       <span className={styles.icon} {...buttonAHoverProps}><SiReact />{buttonAIsHovering ? <span className={styles.iconText}>70%</span> : <span className={styles.iconText}>React</span>}</span>
+       <span className={styles.icon} {...buttonAHoverProps}><SiCss3 />{buttonAIsHovering ? <span className={styles.iconText}>80%</span> : <span className={styles.iconText}>CSS</span>}</span>
+       <span className={styles.icon} {...buttonAHoverProps}><SiTypescript />{buttonAIsHovering ? <span className={styles.iconText}>60%</span> : <span className={styles.iconText}>TS</span>}</span>
+       <span className={styles.icon} {...buttonAHoverProps}><FaNodeJs />{buttonAIsHovering ? <span className={styles.iconText}>60%</span> : <span className={styles.iconText}>Node</span>}</span>
+       <span className={styles.icon} {...buttonAHoverProps}><FaGitSquare />{buttonAIsHovering ? <span className={styles.iconText}>80%</span> : <span className={styles.iconText}>Git</span>}</span>
        </div> 
         </div>
 
